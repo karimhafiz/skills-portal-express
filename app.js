2136c8673f3ac052e49f4a2355ff2bc123364aac
Josh Ferriday <70819699+jferriday@users.noreply.github.com>
2022-04-06 13:23:02 +0100

    use filesystem certs

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // Dependencies
-const express = require("express");
-const morgan = require("morgan")
-const bodyParser = require("body-parser")
-const dotenv = require("dotenv")
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
 const fs = require('fs')
 const spdy = require('spdy')
 const path = require('path')
 
 dotenv.config()
 // Routes
-const {employeeRouter} = require('./routes/employees')
+const { employeeRouter } = require('./routes/employees')
 const { skillsRouter } = require('./routes/skills')
-const {profileRouter} = require('./routes/profile')
+const { profileRouter } = require('./routes/profile')
 
 // Initialise app and middleware
 const app = express()
 app.use(bodyParser.json())
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 
 // Register routes
 app.use('/employees', employeeRouter)
@@ -33,18 +33,23 @@ app.use('/profile', profileRouter)
   cert: fs.readFileSync('/var/ssl/certs/901C65977AC7CD872442CACB865D01B94F9C699F.der'),
         key: fs.readFileSync('/var/ssl/private/901C65977AC7CD872442CACB865D01B94F9C699F.pfx'),
 */
-console.log('----------Certificate and key directory contents: ---------')
-console.log('Certificate Directory:')
-console.log(fs.readdirSync('/var/ssl/certs'))
-console.log('Key Directory:')
-console.log(fs.readdirSync('/var/ssl/private'))
-console.log('-----------------------------------------------------------')
+// console.log('----------Certificate and key directory contents: ---------')
+// console.log('Certificate Directory:')
+// console.log(fs.readdirSync('/var/ssl/certs'))
+// console.log('Key Directory:')
+// console.log(fs.readdirSync('/var/ssl/private'))
+// console.log('-----------------------------------------------------------')
 
 spdy.createServer(
+    // {
+    //     cert: fs.readFileSync('/var/ssl/certs/18201CB636B958839C512D485B6BBCE47DF2721B.der'),
+    //     pfx: fs.readFileSync('/var/ssl/private/18201CB636B958839C512D485B6BBCE47DF2721B.p12'),
+    //     passphrase: '1234'
+    // },
     {
-        cert: fs.readFileSync('/var/ssl/certs/18201CB636B958839C512D485B6BBCE47DF2721B.der'),
-        pfx: fs.readFileSync('/var/ssl/private/18201CB636B958839C512D485B6BBCE47DF2721B.p12'),
-        passphrase: 1234
+        cert: fs.readFileSync('./certificates/server.cer'),
+        pfx: fs.readFileSync('./certificates/server.pfx'),
+        passphrase: '1234'
     },
     app
 ).listen(process.env.PORT, (err) => {
