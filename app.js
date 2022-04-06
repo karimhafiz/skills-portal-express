5458e928f69fea83e8070c6ea757bc967f5b89cf
Josh Ferriday <70819699+jferriday@users.noreply.github.com>
2022-04-06 13:51:34 +0100

    add keyvault thumbprint

--- app.js
+++ app.js
@@ -33,20 +33,21 @@ app.use('/profile', profileRouter)
   cert: fs.readFileSync('/var/ssl/certs/901C65977AC7CD872442CACB865D01B94F9C699F.der'),
         key: fs.readFileSync('/var/ssl/private/901C65977AC7CD872442CACB865D01B94F9C699F.pfx'),
 */
-// console.log('----------Certificate and key directory contents: ---------')
-// console.log('Certificate Directory:')
-// console.log(fs.readdirSync('/var/ssl/certs'))
-// console.log('Key Directory:')
-// console.log(fs.readdirSync('/var/ssl/private'))
-// console.log('-----------------------------------------------------------')
+console.log('----------Certificate and key directory contents: ---------')
+console.log('Certificate Directory:')
+console.log(fs.readdirSync('/var/ssl/certs'))
+console.log('Key Directory:')
+console.log(fs.readdirSync('/var/ssl/private'))
+console.log('-----------------------------------------------------------')
 
 spdy.createServer(
     {
         // cert: fs.readFileSync('/var/ssl/certs/18201CB636B958839C512D485B6BBCE47DF2721B.der'),
-        pfx: fs.readFileSync('/var/ssl/private/18201CB636B958839C512D485B6BBCE47DF2721B.p12'),
-        passphrase: '1234'
+        // pfx: fs.readFileSync('/var/ssl/private/18201CB636B958839C512D485B6BBCE47DF2721B.p12'),
+        pfx: fs.readFileSync('18201CB636B958839C512D485B6BBCE47DF2721B.p12'),
+        passphrase: '1234',
     },
-  
+
     app
 ).listen(process.env.PORT, (err) => {
     if (err) {
